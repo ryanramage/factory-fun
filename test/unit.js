d5edea43a13bf1138da73603586640701f90445d
Ryan Ramage <ryan.ramage@gmail.com>
2018-05-27 23:26:33 -0600

    make a little nicer

--- test/unit.js
+++ test/unit.js
@@ -1,23 +1,26 @@
 var factory = require('../lib/index')
 var test = require('tape')
 
-test('create a realestate factory', function (t) {
-  let team = factory.init()
-  let qualify = {
+test('create a realestate factory', t => {
+  let team = factory.create()
+
+  team.addProcessingStep({
     label: 'qualify',
     recipe: [{ amount: 1, product: 'buyer' }],
     recipeProducts: [{ amount: 1, product: 'buyer.qualified' }],
     effortEstimate: [{ amount: 30, unit: 'minutes' }]
-  }
-  let accumulateListings = {
+  })
+
+  team.addProcessingStep({
     label: 'accumulateListings',
     recipe: [{ amount: 1, product: 'buyer.qualified' }],
     recipeProducts: [{ amount: 1, product: 'showingTour' }],
     effortEstimate: [
       { amount: 3, unit: 'listings', notes: 'this is the number of listings favs accumlated before considering setup' },
       { amount: 20, unit: 'minutes', notes: 'this is the time to setup the showing' }]
-  }
-  let showing = {
+  })
+
+  team.addProcessingStep({
     label: 'showing',
     recipe: [{ amount: 1, product: 'showingTour' }],
     recipeProducts: [{ amount: 3, product: 'completedShowing' }],
@@ -25,29 +28,24 @@ test('create a realestate factory', function (t) {
       { amount: 1, unit: 'hour', notes: 'based on three listings to see' },
       { amount: 50, unit: 'kilometers', notes: 'average driving' }
     ]
-  }
-  let offer = {
+  })
+
+  team.addProcessingStep({
     label: 'offer',
     recipe: [{amount: 1, product: 'completedShowing'}],
     recipeProducts: [{ amount: 1, product: 'acceptedOffer' }],
     effortEstimate: [
       { amount: 3, unit: 'hours', notes: 'to negotiate and get signatures' }
     ]
-  }
+  })
 
+  // connect all the Processing Steps together, joining outputs with next stage inputs
   team
-    .addProcessingStep(qualify)
-    .addProcessingStep(accumulateListings)
     .connectSteps('qualify', 'accumulateListings', [['buyer.qualified', 'buyer.qualified']])
-    .addProcessingStep(showing)
     .connectSteps('accumulateListings', 'showing', [['showingTour', 'showingTour']])
-    .addProcessingStep(offer)
     .connectSteps('showing', 'offer', [['completedShowing', 'completedShowing']])
 
-  let realtor1 = {
-    label: 'e12345'
-  }
-
+  let realtor1 = { label: 'e12345' }
   team
     .addProccessingInstance('qualify', realtor1)
     .addProccessingInstance('accumulateListings', realtor1)
@@ -64,8 +62,8 @@ test('create a realestate factory', function (t) {
   t.end()
 })
 
-test('throw exception if invalid data', function (t) {
-  let brokerage = factory.init()
+test('throw exception if invalid data', t => {
+  let brokerage = factory.create()
   try {
     brokerage.addProcessingStep({ label: 'qualify' })
     t.fail()
@@ -75,8 +73,8 @@ test('throw exception if invalid data', function (t) {
   }
 })
 
-test('test recipe duplicated items throws exception', function (t) {
-  let brokerage = factory.init()
+test('test recipe duplicated items throws exception', t => {
+  let brokerage = factory.create()
   let qualify = {
     label: 'qualify',
     recipe: [{ amount: 1, product: 'buyer' }, { amount: 1, product: 'buyer' }],
@@ -92,8 +90,8 @@ test('test recipe duplicated items throws exception', function (t) {
   }
 })
 
-test('test recipeProducts duplicated items throws exception', function (t) {
-  let team = factory.init()
+test('test recipeProducts duplicated items throws exception', t => {
+  let team = factory.create()
   let qualify = {
     label: 'qualify',
     recipe: [{ amount: 1, product: 'buyer' }],
